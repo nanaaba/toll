5e591def61632e82970966e068ca9489c0deaae5
naby <abaodum@gmail.com>
2017-10-24 13:46:44 +0000

    email implemented

--- public/conf.php
+++ public/conf.php
@@ -5,6 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-require_once '../../tollconf/conf.php'; //holds the DB initializations. 
+//require_once '../../tollconf/conf.php'; //holds the DB initializations. 
 //update for your environment
 
+ORM::configure(array(
+    'connection_string' => 'mysql:host=localhost;dbname=toll',
+    'username' => 'root',
+    'password' => 'password'
+));
