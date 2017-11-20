69c54709ffa67f9fc4b65d62576e8c16ee7742d8
naby <abaodum@gmail.com>
2017-11-20 12:25:09 +0000

    sms integrated

--- public/helpers.php
+++ public/helpers.php
@@ -605,14 +605,14 @@ Password : $password   </br>
 </html>
 ";
 
-        $feedback = sendemail($data['email'], $message);
+          sendemail($data['email'], $message);
 
         $phonemessage = "Username :" . $email . "\n Password :" . $password;
 
-        $feedback = sendMessage($data['contact'], $phonemessage);
+          sendMessage($data['contact'], $phonemessage);
         $dataArray = array(
             "status" => 0,
-            "message" => "User registered successfully" . $feedback
+            "message" => "User registered successfully" 
         );
         return $dataArray;
     }
